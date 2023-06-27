91dbe8a93988a97c5bab32670c18ebab031c69a0
koki-asami <is0392hr@gmail.com>
2023-06-27 16:30:48 +0900

    test

--- setup.py
+++ setup.py
@@ -9,18 +9,18 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-try: # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-    from pip.req import parse_requirements
-from runpy import run_path
+# try: # for pip >= 10
+#     from pip._internal.req import parse_requirements
+# except ImportError: # for pip <= 9.0.3
+#     from pip.req import parse_requirements
+# from runpy import run_path
 
-install_reqs = parse_requirements('requirements.txt', session=False)
-try:  # for pip < 20.1
-    install_requires = [str(ir.req) for ir in install_reqs]
-except AttributeError:  # for pip >= 20.1
-    install_requires = [str(ir.requirement) for ir in install_reqs]
-    print(install_requires, type(install_requires[0]))
+# install_reqs = parse_requirements('requirements.txt', session=False)
+# try:  # for pip < 20.1
+#     install_requires = [str(ir.req) for ir in install_reqs]
+# except AttributeError:  # for pip >= 20.1
+#     install_requires = [str(ir.requirement) for ir in install_reqs]
+#     print(install_requires, type(install_requires[0]))
 
 def get_version():
     namespace = run_path('chumpy/version.py')
